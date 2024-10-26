e5a5b6afe182284dfa5cf7d7a46b1a072386d668
glrmrissi <guilhermerissi04112006@gmail.com>
2024-10-26 18:42:58 -0300

    style changes

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,19 +1,32 @@
-import { styled } from "@mui/material"
+import { Grid, styled } from "@mui/material"
+import Avatar from "../../../../assets/images/avatar.png"
+const Hero = () => {
 
-const Hero = () =>{
+  const StyledHero = styled("div")(() => ({
+    backgroundColor: "black"
+  }))
+  const StyledImg = styled("img")(() => ({
+    width: "100px",
+    height: "100px",
+    borderRadius: "50%"
+  }))
 
-    const StyledHero = styled("div")(() => ({
-        backgroundColor: "black"
-    }))
-
-    return (
-      <>
+  return (
+    <>
       <StyledHero>
         Hero
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <StyledImg src={Avatar} />
+          </Grid>
+          <Grid item xs={9}>
+              <h1>cu</h1>
+          </Grid>
+        </Grid>
       </StyledHero>
-        Hero
-      </>
-    )
-  }
-  
-  export default Hero
+      Hero
+    </>
+  )
+}
+
+export default Hero
